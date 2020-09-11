ee4fb2ff2c7066dcddf3f5df9c1515a3462b2c04
lmas <30lmas09@gmail.com>
2020-09-12 00:08:19 +0530

    'init'

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser')
-require('dotenv/config')
+require('dotenv').config()
 
 app.use(bodyParser.json())
 
 const postrouter = require('./routes/api/posts')
 
 app.get('/' ,(req,res) => {
-    res.send("Heknfaklsdjgn")
+    res.send("Music Player \n Let's enjoy the day")
 })
 
 mongoose.connect(process.env.Config,{useNewUrlParser: true,useUnifiedTopology: true})
